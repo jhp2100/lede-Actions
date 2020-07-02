f8006965708fee0735ebc85d7ffb7e06b98a955f
cqmylx <67597378+cqmylx@users.noreply.github.com>
2020-07-02 20:46:38 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,5 +11,5 @@
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.1.2/g' package/base-files/files/bin/config_generate
 
-
+sed -i 's/OpenWrt/LEDE/g' package/base-files/files/bin/config_generate
 sed -i 's/set luci.main.mediaurlbase/#set luci.main.mediaurlbase/g' feeds/luci/themes/luci-theme-bootstrap/root/etc/uci-defaults/30_luci-theme-bootstrap
