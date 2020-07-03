c91a7e9abd746059c55737ed8cad037ccaf83772
cqmylx <67597378+cqmylx@users.noreply.github.com>
2020-07-03 08:45:35 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -17,3 +17,5 @@ sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 git clone https://github.com/rufengsuixing/luci-app-adguardhome ./package/lean/luci-app-adguardhome
 mkdir ./feeds/luci/applications/luci-app-smartdns
 git clone https://github.com/ujincn/luci-app-smartdns-compat.git ./feeds/luci/applications/luci-app-smartdns
+mkdir ./package/lean/smartdns
+git clone https://github.com/ujincn/smartdns.git ./package/lean/smartdns
